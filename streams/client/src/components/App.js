53eb707866ecd985261c9d341b701981c3ceecf2
jcranwang <jason.cr.wang@gmail.com>
2019-04-02 15:24:37 +1000

    Build and validate create-stream form using redux form

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -13,11 +13,11 @@ const App = () => {
       <BrowserRouter>
         <div>
           <Header />
-          <Route path="/" component={StreamList} />
-          <Route path="/streams/new" component={StreamCreate} />
-          <Route path="/streams/edit" component={StreamEdit} />
-          <Route path="/streams/delete" component={StreamDelete} />
-          <Route path="/streams/show" component={StreamShow} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
