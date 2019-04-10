f3275dd0301f3f41f323172ac57d3c93d20bed86
jcranwang <jason.cr.wang@gmail.com>
2019-04-10 10:15:35 +1000

    Finish streaming functionality with flv.js and rtmpserver

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,22 +1,50 @@
 import React from "react";
 import { connect } from "react-redux";
+import flv from "flv.js";
 import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
+    this.createPlayer();
+  }
+
+  componentDidUpdate() {
+    this.createPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  createPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${this.props.match.params.id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
     if (this.props.stream) {
       return (
         <div>
+          <video ref={this.videoRef} style={{ width: "100%" }} controls />
           <h2>{this.props.stream.title}</h2>
           <h3>{this.props.stream.description}</h3>
         </div>
       );
     } else {
-      return <div>Loading...</div>
+      return <div>Loading...</div>;
     }
   }
 }
