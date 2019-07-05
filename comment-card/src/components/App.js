39db214a806dd4b66ea2c14effd1d7de32ec6dc5
jcranwang <jason.cr.wang@gmail.com>
2019-07-05 16:27:15 +0930

    comment-card: complete the application

--- comment-card/src/components/App.js
+++ comment-card/src/components/App.js
@@ -1,8 +1,36 @@
 import React from "react";
+import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
+import CommentDetails from "./CommentDetails";
 
 const App = () => {
   return (
-    <div>Hello World</div>
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetails
+          avatar={faker.image.avatar()}
+          author={"Jake"}
+          date={"2 days ago"}
+          msg={"Good job!"}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          avatar={faker.image.avatar()}
+          author={"John"}
+          date={"3 days ago"}
+          msg={"Terrific!"}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          avatar={faker.image.avatar()}
+          author={"Jay"}
+          date={"5 days ago"}
+          msg={"Wonderful!"}
+        />
+      </ApprovalCard>
+    </div>
   );
 };
 
