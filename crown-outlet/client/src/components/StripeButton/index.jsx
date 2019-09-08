c41fdca119c8c9da28c8a3fdad2c7cb814d30218
jcranwang <jason.cr.wang@gmail.com>
2019-09-08 11:13:25 +0930

    crown-outlet: built backend server to process Stripe payment

--- crown-outlet/client/src/components/StripeButton/index.jsx
+++ crown-outlet/client/src/components/StripeButton/index.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeButton = ({ price }) => {
   const stripePrice = price * 100;
   const publicKey = process.env.REACT_APP_STRIPE_API_KEY;
-  const onToken = token => {
-    console.log(token);
-    alert("Payment Successfully!");
+
+  const onToken = async token => {
+    try {
+      await axios({
+        url: "payment",
+        method: "post",
+        data: {
+          amount: stripePrice,
+          token
+        }
+      });
+      alert("Payment successfullly");
+    } catch (err) {
+      console.log("Error:", JSON.parse(err));
+      alert("Something wrong with the payment");
+    }
   };
+
   return (
     <StripeCheckout
       label="Pay now"
